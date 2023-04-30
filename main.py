6d6b54f19b32a23734222971ff8cba991562a167
Yaseen Alli <yaseenalli17@gmail.com>
2023-04-30 17:43:28 +0200

    #changing port number

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-#API Hackathon 
-#Yaseen Alli 
-#Alex Urban 
+# API Hackathon 
+# Yaseen Alli 
+# Alex Urban 
 
-#part 2 creating API
-#import libraries 
-
+# Part 2 creating API
+# Import libraries 
 from flask import Flask 
 import json 
 import requests
 import pymysql
 from flask_cors import CORS, cross_origin
-
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -18,12 +16,11 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 from flask import request
 
-
 @app.route('/recipes/<keyword>/<dish_type>/<cuisine_type>/<meal_type>/<calories>', methods=['GET'])
-def get_recipes(keyword, dish_type, cuisine_type, meal_type, calories ):
+def get_recipes(keyword, dish_type, cuisine_type, meal_type, calories):
     print('Request received')
     
-    #connect to MySQL database
+    # Connect to MySQL database
     conn = pymysql.connect(
         host='us-cdbr-east-06.cleardb.net',
         user='b543b1c5defb62',
@@ -33,7 +30,7 @@ def get_recipes(keyword, dish_type, cuisine_type, meal_type, calories ):
         cursorclass=pymysql.cursors.DictCursor
     )
 
-    #build SQL query based on filters
+    # Build SQL query based on filters
     query = "SELECT Name, Image, ROUND(Calories,0), CuisineType, MealType, DishType, CookTime, Ingredients, url FROM Recipes WHERE Name LIKE %s"
     args = ('%' + keyword + '%',) 
 
@@ -41,7 +38,6 @@ def get_recipes(keyword, dish_type, cuisine_type, meal_type, calories ):
         query += " AND (DishType LIKE %s)"
         args += ('%' + dish_type + '%',)
         
-
     if cuisine_type:
         query += " AND (CuisineType LIKE %s)"
         args += ('%' + cuisine_type + '%',)
@@ -54,37 +50,35 @@ def get_recipes(keyword, dish_type, cuisine_type, meal_type, calories ):
         query += " AND (Calories <= %s)"
         args += (calories,)
     
-
-    #execute SQL query to retrieve recipes based on filters
+    # Execute SQL query to retrieve recipes based on filters
     with conn.cursor() as cursor:
         cursor.execute(query, args)
         results = cursor.fetchall()
 
-    #convert results to a dictionary format
-    recipes=[]
+    # Convert results to a dictionary format
+    recipes = []
     for row in results:
-        recipe={
-            'Name':row['Name'],
-            'Image':row['Image'],
-            'Calories':row['ROUND(Calories,0)'],
-            'CuisineType':row['CuisineType'],
-            'MealType':row['MealType'],
-            'DishType':row['DishType'],
-            'CookTime':row['CookTime'],
-            'Ingredients':row['Ingredients'],
-            'url':row['url']
+        recipe = {
+            'Name': row['Name'],
+            'Image': row['Image'],
+            'Calories': row['ROUND(Calories,0)'],
+            'CuisineType': row['CuisineType'],
+            'MealType': row['MealType'],
+            'DishType': row['DishType'],
+            'CookTime': row['CookTime'],
+            'Ingredients': row['Ingredients'],
+            'url': row['url']
         }
 
         recipes.append(recipe)
 
-    #close database connection
+    # Close database connection
     conn.close()
 
-    #return the data in JSON format 
-    return json.dumps({'recipes':recipes})
-
+    # Return the data in JSON format 
+    return json.dumps({'recipes': recipes})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-
-
+    # Set the port number to 5000 (default Flask port number is 5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
