bb138f17260bb669b3d1ba9b5eb5410b9f8c6108
Yaseen Alli <yaseenalli17@gmail.com>
2023-05-06 13:06:21 +0200

    #reverting back to original

--- main.py
+++ main.py
@@ -1,65 +1,80 @@
-from flask import Flask, request, jsonify
-from sqlalchemy import create_engine, text
-from werkzeug.exceptions import BadRequest
+# API Hackathon 
+# Yaseen Alli 
+# Alex Urban 
+
+# Part 2 creating API
+# Import libraries 
+from flask import Flask, jsonify
+import pymysql
+from flask_cors import CORS
 
 app = Flask(__name__)
-cache = {}
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-def get_db_uri():
-    db_config = {
-        "user": "myuser",
-        "password": "mypassword",
-        "host": "localhost",
-        "port": "5432",
-        "database": "recipes"
-    }
-    return f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
-
-@app.route('/recipes/<string:keyword>/<string:dish_type>/<string:cuisine_type>/<string:meal_type>/<int:calories>')
+@app.route('/recipes/<keyword>/<dish_type>/<cuisine_type>/<meal_type>/<calories>', methods=['GET'])
 def get_recipes(keyword, dish_type, cuisine_type, meal_type, calories):
-    if keyword or dish_type or cuisine_type or meal_type or calories:
-        if keyword not in cache:
-            cache[keyword] = {}
+    print('Request received')
+    
+    # Connect to MySQL database
+    conn = pymysql.connect(
+        host='us-cdbr-east-06.cleardb.net',
+        user='b543b1c5defb62',
+        password='28c47692',
+        db='heroku_0fd2f804d4d62c0',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
-        if dish_type not in cache[keyword]:
-            db_uri = get_db_uri()
-            engine = create_engine(db_uri, echo=True)
-
-            query, args = build_query(keyword, dish_type, cuisine_type, meal_type, calories)
-            result = engine.execute(query, args).fetchall()
-
-            cache[keyword][dish_type] = result
-        
-        return jsonify(cache[keyword][dish_type])
-    else:
-        raise BadRequest("At least one parameter should be provided.")
-
-def build_query(keyword, dish_type, cuisine_type, meal_type, calories):
-    query = text("SELECT * FROM recipes WHERE 1=1")
-    args = {}
-
-    if keyword:
-        query = query.where(text("Name LIKE :keyword"))
-        args['keyword'] = f"%{keyword}%"
+    # Build SQL query based on filters
+    query = "SELECT Name, Image, ROUND(Calories,0), CuisineType, MealType, DishType, CookTime, Ingredients, url FROM Recipes WHERE Name LIKE %s"
+    args = ('%' + keyword + '%',) 
 
     if dish_type:
-        query = query.where(text("DishType LIKE :dish_type"))
-        args['dish_type'] = f"%{dish_type}%"
-
+        query += " AND (DishType LIKE %s)"
+        args += ('%' + dish_type + '%',)
+        
     if cuisine_type:
-        query = query.where(text("CuisineType LIKE :cuisine_type"))
-        args['cuisine_type'] = f"%{cuisine_type}%"
+        query += " AND (CuisineType LIKE %s)"
+        args += ('%' + cuisine_type + '%',)
 
     if meal_type:
-        query = query.where(text("MealType LIKE :meal_type"))
-        args['meal_type'] = f"%{meal_type}%"
+        query += " AND (MealType LIKE %s)"
+        args += ('%' + meal_type + '%',)
+    
+    if calories:
+        query += " AND (Calories <= %s)"
+        args += (calories,)
+    
+    # Execute SQL query to retrieve recipes based on filters
+    with conn.cursor() as cursor:
+        cursor.execute(query, args)
+        results = cursor.fetchall()
 
-    if calories is not None:
-        query = query.where(text("Calories <= :calories"))
-        args['calories'] = calories
+    # Convert results to a dictionary format
+    recipes = []
+    for row in results:
+        recipe = {
+            'Name': row['Name'],
+            'Image': row['Image'],
+            'Calories': row['ROUND(Calories,0)'],
+            'CuisineType': row['CuisineType'],
+            'MealType': row['MealType'],
+            'DishType': row['DishType'],
+            'CookTime': row['CookTime'],
+            'Ingredients': row['Ingredients'],
+            'url': row['url']
+        }
 
-    return query, args
+        recipes.append(recipe)
 
+    # Close database connection
+    conn.close()
+
+    # Return the data in JSON format 
+    return jsonify({'recipes': recipes})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Set the port number to 5000 (default Flask port number is 5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
